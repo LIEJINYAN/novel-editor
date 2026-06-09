d58b74244a505019060309ca19d65b476e24c854
liejin <1954730267@qq.com>
2026-06-09 18:41:29 +0800

    fix: Tauri 图标和配置修复

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,9 +3,6 @@
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_dialog::init())
-        .plugin(tauri_plugin_path::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
